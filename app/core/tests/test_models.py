52773ccb520ea93fb47b6e762545463609093780
mjan <mjan@deloitte.ca>
2021-04-13 15:05:18 -0400

    Add tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@mjdevelopers.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -37,3 +43,13 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None)
+
+    def test_new_tag_str(self):
+        """Test the tag self representation"""
+
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan',
+        )
+
+        self.assertEqual(str(tag), tag.name)
---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
+from core.models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag object"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name',)
+        read_only_fields = ('id',)
